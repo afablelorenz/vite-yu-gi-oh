f81efc663eb599ce2144b98cc0dcc258253ffdc1
afablelorenz <afablelorenz@gmail.com>
2024-06-12 18:54:42 +0200

    update

---
+++ src/components/YugiHeader.vue
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<template>
+    <header>
+        <h1>
+            Yu-Gi-Oh API
+        </h1>
+    </header>
+</template>
+
+<style scoped>
+
+header{
+    width: 100%;
+    background-color: 100%;
+    padding: 1rem;
+}
+
+</style>
---
+++ src/components/YugiMain.vue
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <div class="cardContainer">
+                
+            </div>
+        </div>
+        
+    </main>
+</template>
+
+<style>
+
+main{
+    background-color: #D48F38;
+}
+
+div.container{
+    display: flex;
+    justify-content: center;
+    padding: 1rem;
+}
+
+div.cardContainer{
+    height: 89.1vh;
+    background-color: white;
+    width: 80%;
+}
+</style>
