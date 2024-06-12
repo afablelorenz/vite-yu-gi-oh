982d07a13d832f9f78eafa6958ff4cbc441b29aa
afablelorenz <afablelorenz@gmail.com>
2024-06-12 20:21:25 +0200

    update

---
+++ src/components/YugiCards.vue
@@ -0,0 +1,37 @@
+<script>
+export default{
+    components:{
+
+    },
+    data(){
+        return{
+
+        }
+    },
+    props:{
+        yugiCardsList: {
+            type: Array,
+            required: true,
+        }
+    }
+}
+</script>
+
+
+<template>
+    <div>
+        <div>
+            {{ yugiCardsList.length }}
+        </div>
+        <article v-for="card in yugiCardsList" :key="card.name">
+            <img :src="card.card_images[0].image_url">
+            <p></p>
+            <p></p>
+        </article>
+    </div>
+</template>
+
+
+<styles scoped>
+
+</styles>
--- src/components/YugiMain.vue
+++ src/components/YugiMain.vue
@@ -1,12 +1,45 @@
 <script>
+import YugiCards from './YugiCards.vue'
+import axios from 'axios';
+export default{
+    components:{
+        YugiCards,
+    },
+    data(){
+        return{
+            yugiCardsList: [],
+            yugiCardsListImg:[],
+        }
+    },
+    methods:{
+        getYugiCards(){
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+            .then((response) => {
+                console.log(response.data.data);
+                this.yugiCardsList = response.data.data;
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+        },
+        
+    },
 
+    created(){
+        this.getYugiCards();
+    }
+};
 </script>
 
 <template>
     <main>
         <div class="container">
             <div class="cardContainer">
-                
+                <YugiCards :yugiCardsList = "yugiCardsList"/>
             </div>
         </div>
         
